ad0f01cc62f63b1b2ec9fd7039447dcdb8331a9f
Maximiliano Montenegro <maximontenegro@frba.utn.edu.ar>
2026-06-21 12:23:18 -0300

    subo archivo de labo

---
+++ laboratorio_1/script/laboratorio1.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 21 11:29:06 2026
+
+@author: monezemax
+"""
+
+# Inicialización e importación de módulos
+
+# Módulos externos
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as sig
+
+from pytc2.sistemas_lineales import analyze_sys, pretty_print_bicuad_omegayq, tf2sos_analog, pretty_print_SOS, pretty_print_lti
+
+from pytc2.general import print_subtitle
+
+fig_sz_x = 13
+fig_sz_y = 7
+fig_dpi = 80 # dpi
+
+fig_font_size = 11
+
+this_order = 2
+
+mpl.rcParams['figure.figsize'] = (fig_sz_x, fig_sz_y)
+mpl.rcParams['figure.dpi'] = fig_dpi
+plt.rcParams.update({'font.size':fig_font_size})
+
+def sim_aprox(aproxs, orders2analyze, ripple, attenuation):
+
+    all_sys = []
+    filter_names = []
+
+    for (this_aprox, this_order, this_ripple, this_att) in zip(aproxs, orders2analyze, ripple, attenuation):
+
+        if this_aprox == 'Butterworth':
+
+            z,p,k = sig.buttap(this_order)
+
+            eps = np.sqrt( 10**(this_ripple/10) - 1 )
+            num, den = sig.zpk2tf(z,p,k)
+            num, den = sig.lp2lp(num, den, eps**(-1/this_order))
+
+            z,p,k = sig.tf2zpk(num, den)
+
+        elif this_aprox == 'Chebyshev1':
+
+            z,p,k = sig.cheb1ap(this_order, this_ripple)
+
+        elif this_aprox == 'Chebyshev2':
+
+            z,p,k = sig.cheb2ap(this_order, this_att)
+
+        elif this_aprox == 'Bessel':
+
+            z,p,k = sig.besselap(this_order, norm='delay')
+            # Paso a transferencia
+            num, den = sig.zpk2tf(z, p, k)
+
+            # Desnormalización para τg(0)=100  us
+            tau = 100e-6
+            wo = 1/tau
+
+            num, den = sig.lp2lp(num, den, wo)
+
+            # Vuelvo a ZPK para seguir con el código existente
+            z, p, k = sig.tf2zpk(num, den)
+
+        elif this_aprox == 'Cauer':
+
+            z,p,k = sig.ellipap(this_order, this_ripple, this_att)
+
+
+        num, den = sig.zpk2tf(z,p,k)
+
+        
+        all_sys.append(sig.TransferFunction(num,den))
+
+        #this_label = this_aprox + '_ord_' + str(this_order) + '_rip_' + str(this_ripple)+ '_att_' + str(this_att)
+        this_label = this_aprox + '_ord_' + str(this_order)
+        print_subtitle(this_label)
+        # factorizamos en SOS's
+        try:
+            this_sos = tf2sos_analog(num, den)
+        except ValueError:
+            this_sos = sig.tf2sos(num, den, pairing='nearest')
+        
+        pretty_print_lti(num,den)
+        pretty_print_SOS(this_sos, mode='omegayq')        
+        
+        
+        filter_names.append(this_label)
+        
+    # el caracter "_" descarta la salida de la función
+    _ = analyze_sys( all_sys, filter_names )
+
+    return( all_sys, filter_names )
+
+#aprox_name = 'Butterworth'
+#aprox_name = 'Chebyshev1'
+#aprox_name = 'Chebyshev2'
+aprox_name = 'Bessel'
+#aprox_name = 'Cauer'
+
+# parametrizamos el orden para cada aproximación
+orders2analyze = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+
+alpha_max = [1]
+
+# Mismo requerimiento de ripple y atenuación
+aproxs = [aprox_name] * len(orders2analyze)
+ripple = alpha_max * len(orders2analyze) # dB \alpha_{max} <-- Sin parametrizar, lo dejo en Butterworth
+attenuation = [1] * len(orders2analyze) # dB \alpha_{min} <-- Sin parametrizar, att fija
+
+
+print_subtitle('Aproximaciones de Bessel')
+
+( all_sys, filter_names ) = sim_aprox(aproxs, orders2analyze, ripple, attenuation)
+
+for H, nombre in zip(all_sys, filter_names):
+
+    # Barrido de frecuencias
+    w = np.logspace(3, 6, 2000)
+
+    # Respuesta en frecuencia
+    w, h = sig.freqs(H.num, H.den, w)
+
+    # Retardo de grupo
+    fase = np.unwrap(np.angle(h))
+    tau_g = -np.gradient(fase, w)
+
+    # τg a 3 kHz
+    f_test = 3000
+    w_test = 2*np.pi*f_test
+
+    idx = np.argmin(np.abs(w - w_test))
+
+    tau_3k = tau_g[idx]
+    tau_0 = 100e-6
+
+    desvio = abs(tau_3k - tau_0)/tau_0 * 100
+
+    # Atenuación a 5 kHz
+    f_test = 5000
+    w_test = 2*np.pi*f_test
+
+    _, h5 = sig.freqs(H.num, H.den, [w_test])
+
+    att_5k = -20*np.log10(abs(h5[0]))
+
+    print("\n" + "="*50)
+    print(nombre)
+    print(f"τg(3 kHz) = {tau_3k*1e6:.2f} us")
+    print(f"Desvío     = {desvio:.2f} %")
+    print(f"Att(5 kHz) = {att_5k:.2f} dB")
+
+    if desvio <= 2 and att_5k <= 1:
+        print(">>> CUMPLE <<<")
